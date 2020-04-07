a6c909efb49e1ae2f5a1916b474121cc4c14925d
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-04-07 17:34:51 +0200

    Setup Comcast.sdkman

--- initialize.sh
+++ initialize.sh
@@ -2,4 +2,7 @@
 sudo apt-get install -y git python-pip python-dev software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
+
 git clone https://github.com/rschlaefli/homelab.git /home/roland/git/homelab
+
+ansible-galaxy install -r requirements.yml -p roles --force
